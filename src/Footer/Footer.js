6f380713327f0292118e81b3f3e69203fd0fbc8c
L Toan <leanhtoan1191@gmail.com>
2021-06-23 09:56:46 +0700

    Prettier

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,61 +1,66 @@
 import React from "react";
-import "./Footer.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFacebook, faInstagram, faTwitter, faYoutube
-} from '@fortawesome/free-brands-svg-icons'
-import {faChevronDown, faChevronRight} from '@fortawesome/free-solid-svg-icons'
-
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faChevronDown,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   /* footer sns */
   const footerSnsBrands = [
-    {id:1, snsBrand:faFacebook, address:"/#"},
-    {id:2, snsBrand:faInstagram, address:"/#"},
-    {id:3, snsBrand:faTwitter, address:"/#"},
-    {id:4, snsBrand:faYoutube, address:"/#"}
+    { id: 1, snsBrand: faFacebook, address: "/#" },
+    { id: 2, snsBrand: faInstagram, address: "/#" },
+    { id: 3, snsBrand: faTwitter, address: "/#" },
+    { id: 4, snsBrand: faYoutube, address: "/#" },
   ];
   let footerSnsList = footerSnsBrands.map((brand) => {
     return (
-      <li key={brand.id.toString()}
-        className="footer-sns-title">
+      <li key={brand.id.toString()} className="footer-sns-title">
         <a href={brand.address}>
-          <FontAwesomeIcon icon={brand.snsBrand} size="lg"/>
+          <FontAwesomeIcon icon={brand.snsBrand} size="lg" />
         </a>
       </li>
-    )
-
+    );
   });
   /* footer menu */
   const footerMenuTitle = [
-    {id:1, title:"about", address:"/#"},
-    {id:2, title:"feedback", address:"/#"},
-    {id:3, title:"help", address:"/#"},
-    {id:4, title:"terms", address:"/#"},
-    {id:5, title:"privacy", address:"/#"},
-    {id:6, title:"advertise", address:"/#"},
-    ];
+    { id: 1, title: "about", address: "/#" },
+    { id: 2, title: "feedback", address: "/#" },
+    { id: 3, title: "help", address: "/#" },
+    { id: 4, title: "terms", address: "/#" },
+    { id: 5, title: "privacy", address: "/#" },
+    { id: 6, title: "advertise", address: "/#" },
+  ];
   let footerMenuList = footerMenuTitle.map((menu) => {
     return (
       <React.Fragment key={menu.id.toString()}>
-        <li  className="footer-menu-title">
+        <li className="footer-menu-title">
           <a href={menu.address}>{menu.title}</a>
         </li>
-        <li  className="footer-bar">|</li>
+        <li className="footer-bar">|</li>
       </React.Fragment>
-    )
+    );
   });
-  
-  return(
+
+  return (
     <div id="footer">
       <div className="notice-area">
         <div className="notice-detail">
           <a href="/#">
             Notice
-            <em className="icon-chevron-r"><FontAwesomeIcon icon={faChevronRight} /></em>
+            <em className="icon-chevron-r">
+              <FontAwesomeIcon icon={faChevronRight} />
+            </em>
           </a>
           <a href="/#">
-            WEBTOON CANVAS Creator rewards for April 
+            WEBTOON CANVAS Creator rewards for April
             <em className="notice-day">May 31, 2021</em>
           </a>
         </div>
@@ -71,16 +76,16 @@ function Footer() {
           </a>
         </div>
       </div>
-      <ul className="footer-sns">
-        {footerSnsList}
-      </ul>
+      <ul className="footer-sns">{footerSnsList}</ul>
       <ul className="footer-menu">
         {footerMenuList}
         <li className="footer-menu-lang">
           <a href="/#" className="footer-lang">
             <span className="footer-lang-text">English</span>
-            <em className="icon-chevron-d"><FontAwesomeIcon icon={faChevronDown}/></em>
-          </a>  
+            <em className="icon-chevron-d">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </em>
+          </a>
         </li>
       </ul>
       <p className="footer-copyright">ⓒ WEBTOON Entertainment Inc.</p>
@@ -88,7 +93,7 @@ function Footer() {
         <img src="./images/footer-icon.png" alt=""></img>
       </div>
     </div>
-);
+  );
 }
 
 export default Footer;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,31 +2,58 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return(
+  return (
     <div id="nav">
       <div className="nav">
         <div className="nav-linksContainer">
           <a href="/#" className="nav-logo">
-            <img src="./images/logo.png" alt=""/>
+            <img src="./images/logo.png" alt="" />
           </a>
           <ul className="nav-links">
-            <li><a href="/#"><h1>originals</h1></a></li>
-            <li><a href="/#"><h1>genres</h1></a></li>
-            <li><a href="/#"><h1>popular</h1></a></li>
-            <li><a href="/#"><h1>canvas</h1></a></li>
+            <li>
+              <a href="/#">
+                <h1>originals</h1>
+              </a>
+            </li>
+            <li>
+              <a href="/#">
+                <h1>genres</h1>
+              </a>
+            </li>
+            <li>
+              <a href="/#">
+                <h1>popular</h1>
+              </a>
+            </li>
+            <li>
+              <a href="/#">
+                <h1>canvas</h1>
+              </a>
+            </li>
           </ul>
         </div>
         <div className="nav-button">
-          <span className="lk-creator"><a href="/#">Creators 101</a></span>
+          <span className="lk-creator">
+            <a href="/#">Creators 101</a>
+          </span>
           <span className="lk-bar">|</span>
-          <a className="lk-publish" href="/#">Publish</a>
-          <a className="lk-logIn" href="/#">Log In</a>
+          <a className="lk-publish" href="/#">
+            Publish
+          </a>
+          <a className="lk-logIn" href="/#">
+            Log In
+          </a>
           <span className="lk-bar">|</span>
-          <a className="lk-search" href="/#"><img src="https://img.icons8.com/android/24/000000/search.png" alt=""/></a>
+          <a className="lk-search" href="/#">
+            <img
+              src="https://img.icons8.com/android/24/000000/search.png"
+              alt=""
+            />
+          </a>
         </div>
       </div>
     </div>
-);
+  );
 }
 
 export default Header;
