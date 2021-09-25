bda13b4788a72580d758990d476e9d897b263262
leanhtoanqsb <leanhtoan1191@gmail.com>
2021-09-25 16:38:07 +0700

    0925

---
+++ src/listComic/colorMap.js
@@ -0,0 +1,11 @@
+export const genreColorMap = {
+  'drama': '#00b19a',
+  'fantasy': '#8b00e9',
+  'comedy': '#eea800',
+  'action': '#006afa',
+  'romance': '#fd337f',
+  'slice of life': '#9ab710',
+  'thriller': '#c00355',
+  'supernatural': '#7a41e9',
+  'superhero': '#be0000',
+}
